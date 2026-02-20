ca4e3fa3c776f0157047dae40f9138477d482e8b
Pramod Pandey <pndpramod.19@gmail.com>
2026-02-20 16:43:58 -0600

    Initial Structure

---
+++ GroceryManagementSystem.java
@@ -0,0 +1,13 @@
+/**
+ * Grocery Management System
+ * 
+ * It is an inventory program that uses arrays to store item names and stock quantities. 
+ * User can can view inventory and restock items thorugh method calls.
+ * 
+ * This java program demonstrates loops, arrays, conditional logic, and basic user interactions.
+ */
+
+public class GroceryManagementSystem{
+
+    // Team Members please add methods 
+}
