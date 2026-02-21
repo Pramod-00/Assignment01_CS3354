7d07053c449eee41173afd4e53fd1ba368aed4c9
Nathan Pena <nathanjaypena2@gmail.com>
2026-02-20 21:05:44 -0600

    Added Task 1: Inventory Display

--- GroceryManagementSystem.java
+++ GroceryManagementSystem.java
@@ -2,12 +2,23 @@
  * Grocery Management System
  * 
  * It is an inventory program that uses arrays to store item names and stock quantities. 
- * User can can view inventory and restock items thorugh method calls.
+ * User can can view inventory and restock items through method calls.
  * 
  * This java program demonstrates loops, arrays, conditional logic, and basic user interactions.
  */
 
 public class GroceryManagementSystem{
-
-    // Team Members please add methods 
+    /* *
+    * Prints inventory of store by looping through the parallel arrays
+    * @param names Array of item names
+    * @param prices Array of item prices
+    * @param stocks Array of stock inventory
+    */
+    public static void printInventory(String[] names, double[] prices, int[] stocks) {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] != null) {
+                System.out.println("Item: " + names[i] + " Price: $" + prices[i] + " Stock: " + stocks[i]);
+            }
+        }
+    }
 }
