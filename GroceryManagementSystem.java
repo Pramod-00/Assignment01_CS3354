9c4ef76dfdb6f46e3c967f5db8477ba8a68e5a63
Ismah Hassan <ismah@Ismahs-Air.lan>
2026-02-20 21:38:49 -0600

    removed duplicate method for printInventory

--- GroceryManagementSystem.java
+++ GroceryManagementSystem.java
@@ -92,17 +92,4 @@ public static void restock_Item(String[] names, int[] stocks, String target, int
     }
 }
 
-    /* *
-    * Prints inventory of store by looping through the parallel arrays
-    * @param names Array of item names
-    * @param prices Array of item prices
-    * @param stocks Array of stock inventory
-    */
-    public static void printInventory(String[] names, double[] prices, int[] stocks) {
-        for (int i = 0; i < names.length; i++) {
-            if (names[i] != null) {
-                System.out.println("Item: " + names[i] + " Price: $" + prices[i] + " Stock: " + stocks[i]);
-            }
-        }
-    }
 }
